dc353b473d88b2b9fa5d122960d4f3be071a304a
Temi <temioye30@gmail.com>
2022-07-19 18:49:09 +0100

    added ability to get word frequency from string

---
+++ zipf.py
@@ -0,0 +1,17 @@
+import re
+
+word_frequency = {}
+
+def get_word_frequency(word_dict, string, stop_words=[]):
+
+  words = re.split(r"\W+", "the the the sentence it it it it ")
+  words = list(filter(None, words)) #filter returns an iterator that must be converted back to a list
+  
+  for word in words:
+    if(word.lower() in word_dict and word not in stop_words):
+      word_dict[word.lower()] += 1
+    elif word not in stop_words:
+      word_dict[word.lower()] = 1
+
+get_word_frequency(word_frequency, "the the the sentence it it it it ")
+
